12046627674ffef7d4fa95fe8b50134b9d04dd7b
coffeeandpouch <sameinsidler@me.com>
2021-05-23 07:55:17 -0500

    Initial Commit

---
+++ 5o'clock/_o_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  _o_clockApp.swift
+//  5o'clock
+//
+//  Created by Sam Einsidler on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct _o_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
